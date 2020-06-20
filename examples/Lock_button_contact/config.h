b63f71f30c370f31ac64a4a8dead4bfc1af317e0
renandw <50704421+renandw@users.noreply.github.com>
2020-06-20 09:19:45 -0400

    Create config.h

---
+++ examples/Lock_button_contact/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define LED_GPIO 2
+// Which logical level correspond to lit LED (0 or 1)
+#define LED_ACTIVE_LEVEL 0
+
+#define RELAY_GPIO 12
+// Which logical level correspond to closed relay (0 or 1)
+#define RELAY_ACTIVE_LEVEL 1
+
+#define BUTTON_GPIO 0
+// Which logical level correspond to pressed button
+#define BUTTON_ACTIVE_LEVEL 0
+
+#define ACCESSORY_NAME "Lock Button Contact"
+#define ACCESSORY_SETUP_CODE "111-11-111"
+
+#define WIFI_AP_NAME "Lock_Button_contact"
+#define WIFI_AP_PASSWORD "vamosversevai"
