333e59e205d50e6cf068e24e48c5a48529d532b8
renandw <50704421+renandw@users.noreply.github.com>
2020-06-24 10:43:41 -0400

    Create config.h

---
+++ examples/4relays-toggle/config.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#define LED_GPIO 13
+// Which logical level correspond to lit LED (0 or 1)
+#define LED_ACTIVE_LEVEL 0
+
+#define RELAY_GPIO 12
+// Which logical level correspond to closed relay (0 or 1)
+#define RELAY_ACTIVE_LEVEL 1
+
+#define BUTTON_GPIO 0
+// Which logical level correspond to pressed button
+#define BUTTON_ACTIVE_LEVEL 0
+
+#define ACCESSORY_NAME "4Lampadas"
+#define ACCESSORY_SETUP_CODE "736-24-212"
+
+#define WIFI_AP_NAME "4 Lâmpadas"
+#define WIFI_AP_PASSWORD "4lampadas"
